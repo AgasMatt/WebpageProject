528afa05c4a440d98d94163862266b7dfaa6842a
AgasMatt <132637158+AgasMatt@users.noreply.github.com>
2023-05-13 15:30:03 +0800

    Add files via upload

---
+++ signup.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html>
+  <head>
+
+  </head>
+  <body>
+    <form action="" method="post">
+      <table>
+        <tr><td>Username</td><td><input type="text" name="name"></td></tr>
+        <tr><td>Email</td><td><input type="email" name="mail"></td></tr>
+        <tr><td>Password</td><td><input type="password" name="pass"></td></tr>
+        <tr><td>Confirm password</td><td><input type="password" name="confirm"></td></tr>
+      </table>
+      <input type="submit">
+    </form>
+  </body>
+</html>
