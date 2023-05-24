fb987cc6c4890c621906730036238cd728359278
Aikaniceto <132822370+Aikaniceto@users.noreply.github.com>
2023-05-24 10:47:05 +0800

    Add files via upload

---
+++ addtotrtable.php
@@ -0,0 +1,47 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<head>
+	<title>Untitled</title>
+</head>
+<form action="" method="post">
+	<center>
+		<div class="body">
+			<h1></h1>
+			
+			<label><input type="text" name="fname" placeholder=" FIRST NAME" required></label>
+			
+			<label><input type="text" name="mname" placeholder="MIDDLE NAME" required></label>
+			
+			<label><input type="text" name="lname" placeholder="LAST NAME" required></label>
+			
+			<label><input type="text" name="status" placeholder="STATUS" required></label>
+			
+			<label><input type="text" name="birthplace" placeholder="BIRTH PLACE" required></label>
+			
+			<label><input type="text" name="salary" placeholder="SALARY" required></label>
+			
+			<label><input type="date" name="birthdate" id="birthdate"  placeholder="BIRTH DATE" required></label>
+			
+			<label><input type="number" name="" placeholder="" required></label>
+			
+			<label><input type="number" name="" placeholder="" required></label>
+			
+			<label><input type="number" name="" placeholder="" required></label>
+			
+			<label><input type="number" name="" placeholder="" required></label>
+			
+			<label><input type="number" name="" placeholder="" required></label>
+			
+			<label><input type="number" name="" placeholder="" required></label>
+			
+			<input type="submit" value="SUBMIT">
+		</div>
+	</center>
+<body>
+
+<?php
+
+?>
+
+</body>
+</html>
---
+++ admintable.php
@@ -0,0 +1,58 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<head>
+	<title>Untitled</title>
+</head>
+<style>
+	table, th, tr{
+		border-collapse: collapse;
+		height: 4vh;
+		width: 90vw;
+		background: #00A877;
+		margin: 2vh;
+		color: white;
+		text-align: center;
+		font-family:calibri;
+		border: none;
+		border-radius: 15px 15px;
+	}
+  
+	h1{
+		margin-bottom: 20vh;
+	
+		background: #00A877;
+	}
+</style>
+<body>
+<center>
+<h1></h1>	
+<table>
+	<tr>
+		<th>NAME</th>
+		<th>EMAIL</th>
+		<th>USERNAME</th>
+		<th>PASDWORD</th>
+		<th>SECURITY Q</th>
+		<th>ANSWER</th>
+	</tr>
+<?php
+include ("connection.php");
+$result = mysqli_query($connection, "select * from admintble");
+
+if (mysqli_num_rows($result)>0){
+	while ($rows = mysqli_fetch_array($result)){
+		echo "<tr>";
+		echo "<td>". $rows[0]. "</td>";
+		echo "<td>". $rows[1]. "</td>";
+		echo "<td>". $rows[2]. "</td>";
+		echo "<td>". $rows[3]. "</td>";
+		echo "<td>". $rows[4]. "</td>";
+		echo "<td>". $rows[5]. "</td>";
+		echo "</tr>";
+	}
+}
+?>
+</table>
+</center>
+</body>
+</html>
